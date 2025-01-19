ccdd39ae2e074ee19b586d387679e599b3ff9e55
Kris C <kris.kcnotes@gmail.com>
2025-01-19 22:12:27 +1100

    Cleanup, bump version

--- tauri-plugin-wz-reader/examples/tauri-app/src/store/wz.ts
+++
@@ -1,5 +0,0 @@
-import { writable } from 'svelte/store';
-import type { WzReaderAPIClient } from 'tauri-plugin-wz-reader-api';
-
-export const isInitialized = writable(false);
-export const apiClient = writable<WzReaderAPIClient | undefined>(undefined);
